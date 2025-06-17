e21845f1e884a98465540341a6edeca6c244abd0
rawooldipesh <rawooldipesh0@gmail.com>
2025-06-17 18:39:51 +0530

    checking

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-# Install dependencies
+# Upgrade pip and install Python dependencies
+pip install --upgrade pip
 pip install -r requirements.txt
 
-# Force download all required NLTK tokenizers to a known directory
-python -m nltk.downloader -d ./nltk_data punkt punkt_tab stopwords wordnet
+# Download required NLTK data to local directory
+python -m nltk.downloader -d ./nltk_data punkt stopwords wordnet
